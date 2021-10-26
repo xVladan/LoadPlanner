89f37fd1f455ec562e2e57a1ca684c812906e92e
Vladan Andjic <vladanandjic29@gmail.com>
2021-10-26 15:52:13 +0200

    Added methods: entries, add, edit, delete for Customer and TransportStatus. JS.

---
+++ TestApp/Controllers/StatusController.cs
@@ -0,0 +1,75 @@
+﻿using BusinessLogic;
+using DAL.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace TestApp.Controllers
+{
+    public class StatusController : Controller
+    {
+        private MainBLL mainBLL = new MainBLL();
+        // GET: Status
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public JsonResult GetStatus()
+        {
+            try
+            {
+                var status = mainBLL.GetStatus();
+                return Json(status, JsonRequestBehavior.AllowGet);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+        
+        public void AddStatus(TransportStatus statusData)
+        {
+            try
+            {
+                mainBLL.AddStatus(statusData);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void EditStatus(TransportStatus statusData)
+        {
+            try
+            {
+                mainBLL.EditStatus(statusData);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+        public void DeleteStatus(int Id)
+        {
+            try
+            {
+                mainBLL.DeleteStatus(Id);
+            }
+            catch (Exception)
+            {
+
+                throw;
+            }
+        }
+
+    }
+
+}
---
+++ TestApp/Scripts/customer.js
@@ -0,0 +1,183 @@
+﻿$(document).ready(() => {
+
+    insertDataIntoTable();
+});
+
+
+function insertDataIntoTable() {
+    var customer = new DevExpress.data.DataSource({
+        key: 'Id',
+        load: function () {
+            var d = new $.Deferred();
+
+            $.ajax({
+                type: "GET",
+                url: "/Customer/GetCustomers",
+                contentType: "application/json; charset=utf-8",
+                data: "{}",
+                dataType: "json",
+                success: (data) => {
+                    d.resolve(data)
+                },
+                error: (data) => {
+                    d.reject(data);
+                }
+            });
+            return d.promise();
+        },
+        insert: function (values) {
+            $.ajax({
+                url: "/Customer/AddCustomer",
+                type: "POST",
+                data: JSON.stringify({ customerData: values }),
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+            });
+        },
+        update: function (key, values) {
+            let customerArray = customer.items();
+            let editedCustomer = customerArray.find(item => item.Id === key)
+            editedCustomer = {
+                ...editedCustomer,
+                FirstName: values.FirstName ? values.FirstName : editedCustomer.FirstName,
+                LastName: values.LastName ? values.LastName : editedCustomer.LastName,
+                Address: values.Address ? values.Address : editedCustomer.Address,
+                City: values.City ? values.City : editedCustomer.City,
+                Country: values.Country ? values.Country : editedCustomer.Country,
+                Phone: values.Phone ? values.Phone : editedCustomer.Phone,
+                isActive: values.isActive ? values.isActive : editedCustomer.isActive,
+            }
+            $.ajax({
+                url: "/Customer/EditCustomer",
+                type: "POST",
+                data: JSON.stringify(editedCustomer),
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+            });
+        },
+        remove: function (key) {
+            $.ajax({
+                url: "/Customer/DeleteCustomer",
+                type: "POST",
+                data: JSON.stringify({ Id: key }),
+                dataType: 'json',
+                contentType: 'application/json; charset=utf-8',
+            });
+        }
+    });
+
+    $("#dataGrid").dxDataGrid({
+        dataSource: customer,
+        showBorders: true,
+        paging: {
+            pageSize: 10
+        },
+        pager: {
+            visible: true,
+            allowedPageSizes: [10, 15, 50, 100],
+            showPageSizeSelector: true,
+            showInfo: true,
+            showNavigationButtons: true
+        },
+        editing: {
+            mode: "popup",
+            allowUpdating: true,
+            allowDeleting: true,
+            allowAdding: true,
+            popup: {
+                title: "Customer Info",
+                showTitle: true,
+                width: 700,
+                height: 525,
+            },
+            form: {
+                items: [
+                    {
+                        itemType: "group",
+                        colCount: 2,
+                        colSpan: 2,
+                        items: [
+                            {
+                                dataField: "FirstName",
+                                colSpan: 2,
+                            },
+                            {
+                                dataField: "LastName",
+                                colSpan: 2,
+                            },
+                            {
+                                dataField: "Address",
+                                colSpan: 2,
+                            },
+                            {
+                                dataField: "City",
+                                colSpan: 2,
+                            },
+                            {
+                                dataField: "Country",
+                                colSpan: 2,
+                            },
+                            {
+                                dataField: "Phone",
+                                colSpan: 2,
+                            },
+                            {
+                                dataField: "isActive",
+                                colSpan: 2,
+                            },
+                        ]
+                    },
+                ]
+            },
+        },
+        columns: [
+            {
+                dataField: "Id",
+                caption: "Number",
+                width: "6%",
+                dataType: "text",
+                visible: false,
+                allowEditing: false,
+                formItem: {
+                    visible: false
+                }
+            },
+            {
+                dataField: "FirstName",
+                width: "15%",
+                dataType: "text",
+            },
+            {
+                dataField: "LastName",
+                width: "15%",
+                dataType: "text",
+            },
+            {
+                dataField: "Address",
+                width: "15%",
+                dataType: "text",
+            },
+            {
+                dataField: "City",
+                width: "15%",
+                dataType: "text",
+            },
+            {
+                dataField: "Country",
+                width: "15%",
+                dataType: "text",
+            },
+            {
+                dataField: "Phone",
+                width: "15%",
+                dataType: "text",
+            },
+            {
+                dataField: "isActive",
+                width: "8%",
+                dataType: "boolean",
+            },
+        ],
+    });
+
+}
